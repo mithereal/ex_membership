dd3d4a9e2f800fd550cf7626253cf186e784ef8f
mithereal <mithereal@gmail.com>
2023-07-31 14:11:29 -0700

    refactor

---
+++ priv/repo/migrations/20230731217066_create_plan_features_table.exs
@@ -0,0 +1,14 @@
+defmodule Membership.Repo.Migrations.CreatePlanFeaturesTable do
+  use Ecto.Migration
+
+  def change do
+    key_type = Membership.Config.key_type(:migration)
+
+    create table(:membership_plan_features, primary_key: false) do
+      add(:id, key_type, primary_key: true)
+
+      add(:plan_id, references(Membership.Plan.table()))
+      add(:feature_id, references(Membership.Feature.table()))
+    end
+  end
+end
