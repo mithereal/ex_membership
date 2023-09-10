ebcdd341ed0aa4b43f3c0dccccaad651239eac58
mithereal <mithereal@gmail.com>
2023-09-09 20:14:39 -0700

    .

--- priv/repo/migrations/20230908214049_create_membership_roles_table.exs
+++ priv/repo/migrations/20230908214049_create_membership_roles_table.exs
@@ -4,7 +4,7 @@ defmodule Membership.Repo.Migrations.CreateMemberRolesTable do
   def change do
     key_type = Membership.Config.key_type(:migration)
 
-    create table(:membership_member_plans, primary_key: false) do
+    create table(:membership_member_roles, primary_key: false) do
       add(:member_id, references(Membership.Member.table(), type: key_type))
       add(:role_id, references(Membership.Role.table(), type: key_type))
     end
