d7d4ba8b7c67be0b56672fa9ba7ef2291b671648
mithereal <mithereal@gmail.com>
2023-10-07 17:43:33 -0700

    .

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,9 +1,7 @@
-alias Membership.Repo
-
 feature_1 = Membership.Feature.create("Author Document", "Author Document")
 feature_2 = Membership.Feature.create("change settings", "change settings")
 
 plan = Membership.Plan.create("Bronze", "Bronze Plan")
 
 Membership.Feature.grant(feature_1, plan)
-Membership.Member.grant(feature_2, plan)
+Membership.Feature.grant(feature_2, plan)
