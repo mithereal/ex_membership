653988c8aa39d37fa40e3b8034bd4710df2de679
mithereal <mithereal@gmail.com>
2023-11-05 18:18:08 -0700

    .

--- test/mix/tasks/membership.setup_test.exs
+++ test/mix/tasks/membership.setup_test.exs
@@ -1,6 +1,6 @@
 defmodule Mix.Tasks.MembershipSetupTest do
   use ExUnit.Case
-  import Mock
+  # import Mock
   # doesnt work with circle ci
   #  test "provide a list of available membership mix tasks" do
   #    with_mock Mix.Tasks.Ecto.Migrate, run: fn _params -> nil end do
